9f05ed3d0f3c6d0868753e05aedaf0ad86eaafa9
alexvanin <alexey@nspcc.ru>
2020-07-24 17:46:29 +0300

    Update neofs-node to v0.11.0
    This commit:
      - removes unused .drone.yml,
      - updates neofs-node and neofs-cli up to v0.11.0,
      - updates chain dumps.

--- services/morph_chain/artifacts.mk
+++ services/morph_chain/artifacts.mk
@@ -7,7 +7,7 @@ get.morph_chain:
 ifeq (${MORPH_CHAIN_PATH},)
 	@echo "⇒ Download morph chain dump VERSION=${MORPH_CHAIN_VERSION}"
 	@curl \
-		-sS "https://fs.neo.org/dist/morph_chain.gz" \
+		-sS "https://cdn.fs.neo.org/5iZZi4HzXpk3xjHa2ahi8RCrVae74Q7487ns5ms7S2EF/8c3f3cba-8426-48b2-8493-c09da7539681" \
 		-o ./vendor/morph_chain.gz
 else
 	@echo "⇒ Copy local archive ${MORPH_CHAIN_PATH}"
