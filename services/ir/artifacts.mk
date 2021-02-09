ec984717b032cd1c9da6723f212a0c95084afc87
Leonard Lyubich <leonard@nspcc.ru>
2021-02-09 18:57:37 +0300

    [#46] services/ir: Add NeoFS LOCODE database to IR volumes
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ services/ir/artifacts.mk
@@ -0,0 +1,20 @@
+# Get NeoFS LOCODE database
+
+LOCODE_DB_ARCHIVE_PATH=./vendor
+LOCODE_DB_ARCHIVE_FILE=locode_db.gz
+
+get.ir: LOCODE_DB_PATH?=
+get.ir:
+	@mkdir -p ${LOCODE_DB_ARCHIVE_PATH}
+
+ifeq (${LOCODE_DB_PATH},)
+	@echo "⇒ Download NeoFS LOCODE database from ${CHAIN_URL}"
+	@curl \
+		-sSL "${LOCODE_DB_URL}" \
+		-o ${LOCODE_DB_ARCHIVE_PATH}/${LOCODE_DB_ARCHIVE_FILE}
+else
+	@echo "⇒ Copy local archive of NeoFS LOCODE database from ${LOCODE_DB_PATH}"
+	@cp ${LOCODE_DB_PATH} ${LOCODE_DB_ARCHIVE_PATH}/${LOCODE_DB_ARCHIVE_FILE}
+endif
+
+	gzip -dk ${LOCODE_DB_ARCHIVE_PATH}/${LOCODE_DB_ARCHIVE_FILE}
