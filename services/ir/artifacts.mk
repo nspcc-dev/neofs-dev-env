9d656c3c86e35071e20ac4e5257b2df7b1217662
Alex Vanin <alexey@nspcc.ru>
2021-06-29 22:27:54 +0300

    [#106] serivce/ir: Add NeoFS CLI binary in required artifacts
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- services/ir/artifacts.mk
+++ services/ir/artifacts.mk
@@ -3,8 +3,10 @@
 LOCODE_DB_ARCHIVE_PATH=./vendor
 LOCODE_DB_ARCHIVE_FILE=locode_db.gz
 
-get.ir: LOCODE_DB_PATH?=
-get.ir:
+get.ir: get.locode get.cli
+
+get.locode: LOCODE_DB_PATH?= 
+get.locode:
 	@mkdir -p ${LOCODE_DB_ARCHIVE_PATH}
 
 ifeq (${LOCODE_DB_PATH},)
@@ -18,3 +20,23 @@ else
 endif
 
 	gzip -dfk ${LOCODE_DB_ARCHIVE_PATH}/${LOCODE_DB_ARCHIVE_FILE}
+
+.ONESHELL:
+get.cli: NEOFS_CLI_FILE=./vendor/neofs-cli
+get.cli: NEOFS_CLI_ARCHIVE_FILE=${NEOFS_CLI_FILE}.tar.gz
+get.cli: NEOFS_CLI_PATH?=
+get.cli:
+	@mkdir -p ./vendor
+
+ifeq (${NEOFS_CLI_PATH},)
+	@echo "⇒ Download NeoFS CLI binary from ${NEOFS_CLI_URL}"
+	@curl \
+		-sSL "${NEOFS_CLI_URL}" \
+		-o ${NEOFS_CLI_ARCHIVE_FILE} 
+	@tar -xvf ${NEOFS_CLI_ARCHIVE_FILE} -C ./vendor | xargs -I {} \
+		mv ./vendor/{} ${NEOFS_CLI_FILE}
+	@rm ${NEOFS_CLI_ARCHIVE_FILE}
+else
+	@echo "⇒ Copy local binary from ${NEOFS_CLI_PATH}"
+	@cp ${NEOFS_CLI_PATH} ${NEOFS_CLI_FILE}
+endif
