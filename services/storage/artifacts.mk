a79acec742922afd82668d1d2818dda59f59584e
Denis Kirillov <denis@nspcc.ru>
2021-07-06 12:52:22 +0300

    [#90] Added tls cert to nodes
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- services/storage/artifacts.mk
+++ services/storage/artifacts.mk
@@ -10,6 +10,8 @@ get.storage:
       echo "req_extensions=san"; \
       echo "[san]"; \
       echo "subjectAltName=DNS:s04.${LOCAL_DOMAIN}") > ${SSL_CONFIG}
+ifeq ($(shell ! test -e ${STORAGE_DIR}/s04tls.key && echo -n yes),yes)
 	@openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
-		-subj "/C=RU/ST=SPB/L=St.Petersburg/O=NSPCC/OU=NSPCC/CN=s04.${LOCAL_DOMAIN}" \
-		-keyout ${STORAGE_DIR}/s04tls.key -out ${STORAGE_DIR}/s04tls.crt -extensions san -config ${SSL_CONFIG}
+    		-subj "/C=RU/ST=SPB/L=St.Petersburg/O=NSPCC/OU=NSPCC/CN=s04.${LOCAL_DOMAIN}" \
+    		-keyout ${STORAGE_DIR}/s04tls.key -out ${STORAGE_DIR}/s04tls.crt -extensions san -config ${SSL_CONFIG}
+endif
