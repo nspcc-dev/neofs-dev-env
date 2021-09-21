28f9083e9fc1c4a1bec79dd05a5d27a2d3b3bd95
Alex Vanin <alexey@nspcc.ru>
2021-09-21 17:56:19 +0300

    [#47] Add script to resolve side chain contract addresses from NNS
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ bin/resolve.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Source env settings
+. .env
+
+# NeoGo binary path.
+NEOGO="${NEOGO:-docker exec -it morph_chain neo-go}"
+# NNS contract script hash
+NNS_ADDR=`curl -s --data '{ "id": 1, "jsonrpc": "2.0", "method": "getcontractstate", "params": [1] }' http://morph_chain.${LOCAL_DOMAIN}:30333/ | jq -r '.result.hash'`
+
+${NEOGO} contract testinvokefunction \
+-r http://morph_chain.${LOCAL_DOMAIN}:30333 \
+${NNS_ADDR} resolve string:${1} int:16 | jq -r '.stack[0].value' | base64 -d
