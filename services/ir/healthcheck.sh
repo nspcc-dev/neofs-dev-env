a7feea730a4750f8ebe1976c5fc4581961b23151
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-10-20 10:32:15 +0300

    [#135] .env: update neofs-node version
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- services/ir/healthcheck.sh
+++ services/ir/healthcheck.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-EXPECTED="Health status: READY"
-GOT=`/neofs-cli -r $NEOFS_IR_CONTROL_GRPC_ENDPOINT --binary-key /wallet01.key control healthcheck --ir | grep Health`
-if [ "$EXPECTED" = "$GOT" ]; then exit 0; else exit 1; fi
+/neofs-cli control healthcheck \
+	--endpoint "$NEOFS_IR_CONTROL_GRPC_ENDPOINT" \
+	--binary-key /wallet01.key --ir |
+	grep "Health status: READY"
