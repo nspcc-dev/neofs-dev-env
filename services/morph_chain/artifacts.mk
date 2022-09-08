8c6f321e5f4d0397870c907005e7bdae0e69e794
Alex Vanin <a.vanin@yadro.com>
2022-09-08 18:18:53 +0300

    [#208] Download contracts and neofs-adm
    These artifacts are required for environment deploy.
    
    Signed-off-by: Alex Vanin <a.vanin@yadro.com>

--- services/morph_chain/artifacts.mk
+++ services/morph_chain/artifacts.mk
@@ -1,4 +1,5 @@
 # Download NeoFS sidechain dump with pre-deployed NeoFS contracts
+get.morph_chain: get.contracts get.adm
 get.morph_chain: MORPH_CHAIN_DUMP_NAME=neo.morph.dump.
 get.morph_chain: MORPH_CHAIN_PATH?=
 get.morph_chain:
@@ -12,4 +13,34 @@ ifeq (${MORPH_CHAIN_PATH},)
 else
 	@echo "⇒ Copy local archive ${MORPH_CHAIN_PATH}"
 	@cp ${MORPH_CHAIN_PATH} ./vendor/morph_chain.gz
+endif
+
+get.contracts: NEOFS_CONTRACTS_DEST=./vendor/contracts
+get.contracts: NEOFS_CONTRACTS_ARCHIVE=neofs-contracts.tar.gz
+get.contracts:
+	@mkdir -p ${NEOFS_CONTRACTS_DEST}
+
+ifeq (${NEOFS_CONTRACTS_PATH},)
+	@echo "⇒ Download compiled NeoFS contracts from ${NEOFS_CONTRACTS_URL}"
+	@curl -sSL ${NEOFS_CONTRACTS_URL} -o ${NEOFS_CONTRACTS_ARCHIVE}
+	@tar -xf ${NEOFS_CONTRACTS_ARCHIVE} -C ${NEOFS_CONTRACTS_DEST} --strip-components 1 
+	@rm ${NEOFS_CONTRACTS_ARCHIVE}
+else
+	@echo "⇒ Copy compiled contracts from ${NEOFS_CONTRACTS_PATH}"
+	@cp -r ${NEOFS_CONTRACTS_PATH} ${NEOFS_CONTRACTS_DEST}
+endif
+
+get.adm: NEOFS_ADM_DEST=./vendor/neofs-adm
+get.adm: NEOFS_ADM_ARCHIVE=neofs-adm.tar.gz
+get.adm:
+
+ifeq (${NEOFS_ADM_PATH},)
+	@echo "⇒ Download NeoFS ADM binary from ${NEOFS_ADM_URL}"
+	@curl -sSL ${NEOFS_ADM_URL} -o ${NEOFS_ADM_ARCHIVE}
+	@tar -xvf ${NEOFS_ADM_ARCHIVE} -C ./vendor | xargs -I {} \
+		mv ./vendor/{} ${NEOFS_ADM_DEST}
+	@rm ${NEOFS_ADM_ARCHIVE}
+else
+	@echo "⇒ Copy neofs-adm binary from ${NEOFS_ADM_PATH}"
+	@cp ${NEOFS_ADM_PATH} ${NEOFS_ADM_DEST}
 endif
