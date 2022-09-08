aacbb8f82f4bafc2f7e8f3ef485a7b99eeb8e4d2
Alex Vanin <a.vanin@yadro.com>
2022-09-08 18:26:04 +0300

    [#208] Use neofs-adm to deploy sidechain environment
    Signed-off-by: Alex Vanin <a.vanin@yadro.com>

--- services/morph_chain/artifacts.mk
+++ services/morph_chain/artifacts.mk
@@ -1,19 +1,5 @@
-# Download NeoFS sidechain dump with pre-deployed NeoFS contracts
+# Download NeoFS Contracts and Adm tool to deploy environment
 get.morph_chain: get.contracts get.adm
-get.morph_chain: MORPH_CHAIN_DUMP_NAME=neo.morph.dump.
-get.morph_chain: MORPH_CHAIN_PATH?=
-get.morph_chain:
-	@mkdir -p ./vendor
-
-ifeq (${MORPH_CHAIN_PATH},)
-	@echo "⇒ Download morph chain dump from ${MORPH_CHAIN_URL}"
-	@curl \
-		-sSL "${MORPH_CHAIN_URL}" \
-		-o ./vendor/morph_chain.gz
-else
-	@echo "⇒ Copy local archive ${MORPH_CHAIN_PATH}"
-	@cp ${MORPH_CHAIN_PATH} ./vendor/morph_chain.gz
-endif
 
 get.contracts: NEOFS_CONTRACTS_DEST=./vendor/contracts
 get.contracts: NEOFS_CONTRACTS_ARCHIVE=neofs-contracts.tar.gz
