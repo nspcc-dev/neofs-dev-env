2181ac335885c9fce2034bcfaf2ae05760baf0bc
Roman Khimov <roman@nspcc.ru>
2024-03-21 19:05:25 +0300

    bin: add awaiting to config.sh
    Technically, it's racy. We call tick.sh below and it pushes a transaction of
    its own. They can end up in the same block and then the order would depend on
    fees, so we can have epoch tick first and config change afterwards.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- bin/config.sh
+++ bin/config.sh
@@ -39,7 +39,7 @@ echo "Changing ${KEY} configuration value to ${VALUE}"
 # shellcheck disable=SC2086
 ${NEOGO} contract invokefunction \
 	--wallet-config ${CONFIG_IMG} \
-	-a ${ADDR} --force \
+	-a ${ADDR} --force --await \
 	-r http://ir01.${LOCAL_DOMAIN}:30333 \
 	${NETMAP_ADDR} \
 	setConfig bytes:beefcafe \
