4ff9b7ce864f178d48a90b5c57f7b267dd4a2275
Pavel Karpy <carpawell@nspcc.ru>
2026-02-25 18:47:15 +0300

    services/neo-go: do not copy local binary as a directory
    Likely never worked correctly and was copied from local contracts override
    routine from `ir/artifacts.mk`. It is likely not expected to copy every file
    from the path where `neo-go` is placed.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- services/neo-go/artifacts.mk
+++ services/neo-go/artifacts.mk
@@ -11,6 +11,6 @@ ifeq (${NEO_GO_PATH},)
 	@curl -sSL ${NEO_GO_URL} -o ${NEO_GO_DEST}/${NEO_GO_BIN}
 else
 	@echo "⇒ Copy compiled neo-go from ${NEO_GO_PATH}"
-	@cp -r ${NEO_GO_PATH}/* ${NEO_GO_DEST}
+	@cp -r ${NEO_GO_PATH} ${NEO_GO_DEST}
 endif
 	@chmod +x ${NEO_GO_DEST}/${NEO_GO_BIN}
